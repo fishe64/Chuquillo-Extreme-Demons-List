38a49063c34ff74c0662a58db9e4be3428853540
Tivuron23 <168941653+Tivuron23@users.noreply.github.com>
2024-12-30 16:14:54 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,16 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        No usar hacks (Máximo 360 FPS, CBF y Physics Bypass se pueden usar)
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        El récord debe ser en la ID del nivel mencionado en la lista. No puede ser una copia modificada.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Clicks que se puedan escuchar con claridad. No se puede editar el audio.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        No usar secret ways.
                     </p>
                 </div>
             </div>
